73b58f3879e5151b90335aa42920bdb68f59491d
Artur Milancej <a.milancej@gmail.com>
2019-10-28 18:59:31 +0100

    Added first navbar and content layout css, removed footer

--- frontend/FrontendApp/src/app/app.module.ts
+++ frontend/FrontendApp/src/app/app.module.ts
@@ -3,17 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
