b68020ee89e617dff3c76888418e208870901c25
Artur Milancej <a.milancej@gmail.com>
2019-10-28 17:21:03 +0100

    initial commit

---
+++ backend/BackendApp/src/test/java/pl/milancej/BackendApp/BackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milancej.BackendApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
