83be7bfbcefae617bc560d96a723c9cafa94c2ae
Artur Milancej <a.milancej@gmail.com>
2019-10-29 14:25:42 +0100

    added base layout

--- frontend/FrontendApp/src/app/app.module.ts
+++ frontend/FrontendApp/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
   declarations: [
@@ -12,8 +11,6 @@ import { FlexLayoutModule } from '@angular/flex-layout'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
