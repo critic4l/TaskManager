a5550b55c401022d05c7076874745db0b826a459
Artur Milancej <a.milancej@gmail.com>
2019-10-31 22:47:44 +0100

    Added possibility to create new table

--- backend/BackendApp/src/main/java/pl/milancej/BackendApp/TableController.java
+++ backend/BackendApp/src/main/java/pl/milancej/BackendApp/TableController.java
@@ -1,8 +1,6 @@
 package pl.milancej.BackendApp;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -12,26 +10,31 @@ import pl.milancej.BackendApp.mappers.TableMapper;
 import java.util.List;
 import java.util.Map;
 
-@Controller
-@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@CrossOrigin(origins = "http://localhost:5555")
 @RequestMapping(path = "/table")
 public class TableController {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    private final Logger logger= LoggerFactory.getLogger(TableController.class);
+    private final String TABLE_NAME = "task_table";
+
 
     @GetMapping(path = "/getAllTables")
     public @ResponseBody
     List<Map<String, Object>> getAllTables() {
-        //logger.info("Getting All Tables");
-        return jdbcTemplate.queryForList("SELECT * FROM `task_table`");
+        return jdbcTemplate.queryForList(String.format("SELECT * FROM `%s`", TABLE_NAME));
     }
 
     @GetMapping(path = "/getTableByName")
-    public @ResponseBody Object getTableByName(@RequestParam String name) {
-        //logger.info("Getting table by name: " + name);
-        return jdbcTemplate.queryForObject("SELECT * FROM task_table WHERE name=?", new Object[]{ name }, new TableMapper());
+    public @ResponseBody
+    Object getTableByName(@RequestParam String name) {
+        return jdbcTemplate.queryForObject(String.format("SELECT * FROM %s WHERE name=?", TABLE_NAME), new Object[]{name}, new TableMapper());
+    }
+
+    @PostMapping(path = "/createTable")
+    public void createTable(@RequestParam String name, @RequestParam String description) {
+        jdbcTemplate.update(String.format("INSERT INTO `%s` (name, description) VALUES (?,?)", TABLE_NAME), name, description);
     }
 }
