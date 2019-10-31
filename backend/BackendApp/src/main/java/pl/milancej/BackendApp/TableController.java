1e54e796178454899fc1cfc708c7693fca410179
Artur Milancej <a.milancej@gmail.com>
2019-10-31 17:04:10 +0100

    Added taskController

--- backend/BackendApp/src/main/java/pl/milancej/BackendApp/TableController.java
+++ backend/BackendApp/src/main/java/pl/milancej/BackendApp/TableController.java
@@ -3,18 +3,15 @@ package pl.milancej.BackendApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import pl.milancej.BackendApp.entities.Table;
+import org.springframework.web.bind.annotation.*;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Map;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "/table")
 public class TableController {
 
@@ -22,12 +19,17 @@ public class TableController {
     private JdbcTemplate jdbcTemplate;
 
     @GetMapping(path = "/getAllTables")
-    public @ResponseBody List<Map<String, Object>> getAllTables() {
+    public @ResponseBody
+    List<Map<String, Object>> getAllTables() {
         return jdbcTemplate.queryForList("SELECT * FROM `task_table`");
     }
 
     @GetMapping(path = "/getTableByName")
-    public @ResponseBody List<Map<String, Object>> getTableByName(@RequestParam String name) {
-        return jdbcTemplate.queryForList(String.format("SELECT name, description FROM task_table WHERE name = '%s'", name));
+    public @ResponseBody
+    List<Map<String, Object>> getTableByName(@RequestParam String name) {
+        return jdbcTemplate.queryForList("SELECT name, description FROM task_table WHERE name = :name",
+                new MapSqlParameterSource()
+                        .addValue("name", name)
+        );
     }
 }
---
+++ backend/BackendApp/src/main/java/pl/milancej/BackendApp/TaskController.java
@@ -0,0 +1,31 @@
+package pl.milancej.BackendApp;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Controller
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(path = "/task")
+public class TaskController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping(path = "/getTask")
+    public @ResponseBody Map<String, Object> getTaskById(@RequestParam long id) {
+//        return jdbcTemplate.query("SELECT * FROM task WHERE id = :id",
+//                new MapSqlParameterSource());
+        return null;
+    }
+
+    @GetMapping(path = "/getTask")
+    public @ResponseBody Map<String, Object> getTaskByName(@RequestParam String name) {
+        return null;
+    }
+}
