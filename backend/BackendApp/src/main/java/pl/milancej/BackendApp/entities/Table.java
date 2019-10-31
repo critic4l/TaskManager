ad4fc9acb5ca6a7eac350c62ca4288b900b4cfd3
Artur Milancej <a.milancej@gmail.com>
2019-10-31 15:42:05 +0100

    Added first controller functions

---
+++ backend/BackendApp/src/main/java/pl/milancej/BackendApp/entities/Table.java
@@ -0,0 +1,21 @@
+package pl.milancej.BackendApp.entities;
+
+public class Table {
+    private long id;
+    private String name;
+    private String description;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
