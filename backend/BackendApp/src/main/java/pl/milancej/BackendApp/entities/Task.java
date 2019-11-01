127afe59e775012778c732863f233822e386eea5
Artur Milancej <a.milancej@gmail.com>
2019-11-01 16:47:10 +0100

    Added task entity and taskMapper

---
+++ backend/BackendApp/src/main/java/pl/milancej/BackendApp/entities/Task.java
@@ -0,0 +1,49 @@
+package pl.milancej.BackendApp.entities;
+
+public class Task {
+    private long id;
+    private long table_id;
+    private long user_id;
+    private String title;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getTable_id() {
+        return table_id;
+    }
+
+    public void setTable_id(long table_id) {
+        this.table_id = table_id;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ backend/BackendApp/src/main/java/pl/milancej/BackendApp/mappers/TaskMapper.java
@@ -0,0 +1,20 @@
+package pl.milancej.BackendApp.mappers;
+
+import org.springframework.jdbc.core.RowMapper;
+import pl.milancej.BackendApp.entities.Task;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TaskMapper implements RowMapper {
+    @Override
+    public Object mapRow(ResultSet resultSet, int i) throws SQLException {
+        Task task = new Task();
+        task.setId(resultSet.getLong("id"));
+        task.setTable_id(resultSet.getLong("table_id"));
+        task.setUser_id(resultSet.getLong("user_id"));
+        task.setTitle(resultSet.getString("title"));
+        task.setDescription(resultSet.getString("description"));
+        return task;
+    }
+}
